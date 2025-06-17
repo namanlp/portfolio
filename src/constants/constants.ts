8afc292f8af7a7db552db22ebfd99918dfd71f4f
Naman Garg <ngarg1270@gmail.com>
2025-06-17 22:10:33 +0530

    Update Some variables

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -5,12 +5,12 @@ export const LIGHT_MODE = "light",
 export const DEFAULT_THEME = DARK_MODE;
 
 // Banner height unit: vh
-export const BANNER_HEIGHT = 35;
+export const BANNER_HEIGHT = 40;
 export const BANNER_HEIGHT_EXTEND = 30;
 export const BANNER_HEIGHT_HOME = BANNER_HEIGHT + BANNER_HEIGHT_EXTEND;
 
 // The height the main panel overlaps the banner, unit: rem
-export const MAIN_PANEL_OVERLAPS_BANNER_HEIGHT = 3.5;
+export const MAIN_PANEL_OVERLAPS_BANNER_HEIGHT = 5;
 
 // Page width: rem
-export const PAGE_WIDTH = 75;
+export const PAGE_WIDTH = 100;
